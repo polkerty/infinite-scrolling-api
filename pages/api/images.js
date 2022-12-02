7bf1f490523a6a726024d3cd220d05b5cf2160f3
Jacob Brazeal <jacob.brazeal@scale.com>
2022-12-01 17:19:24 -0800

    /api/images

--- pages/api/images.js
+++ pages/api/images.js
@@ -1,7 +1,13 @@
-
+import data from './data.json'
 
 export default function handler(req, res) {
-
-
-  res.status(200).json({ name: 'John Doe' })
+    const ret = [];
+    const nextToken = req.query.nextToken || data[0].id;
+    const count = req.query.count ? parseInt(req.query.count) : 10;
+    const firstIndex = data.findIndex(item => item.id === nextToken);
+    for (let i = 0; i < count; ++i) {
+        ret.push(data[(firstIndex + i) % data.length]);
+    }
+    const returnNextToken = data[(firstIndex + count) % data.length].id;
+    res.status(200).json({data: ret, nextToken: returnNextToken})
 }
