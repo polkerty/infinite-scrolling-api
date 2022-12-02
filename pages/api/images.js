d2249e50f8960d743a30dd37fe0caa812123e0b5
Jacob Brazeal <jacob.brazeal@scale.com>
2022-12-02 14:05:57 -0800

    fix cors errors

--- pages/api/images.js
+++ pages/api/images.js
@@ -1,6 +1,24 @@
 import data from './data.json'
 
-export default function handler(req, res) {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+
+function handler(req, res) {
     const ret = [];
     const nextToken = req.query.nextToken || data[0].id;
     const count = req.query.count ? parseInt(req.query.count) : 10;
@@ -11,3 +29,6 @@ export default function handler(req, res) {
     const returnNextToken = data[(firstIndex + count) % data.length].id;
     res.status(200).json({data: ret, nextToken: returnNextToken})
 }
+
+module.exports = allowCors(handler)
+
